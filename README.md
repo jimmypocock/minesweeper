1fcb498e5eb493d68fc5d4aa7403755c3e0d780b
Jimmy Pocock <jimmypocock@yahoo.com>
2014-09-16 17:07:56 -0500

    Split css, js, and html. Updated READ ME.

---
+++ README.md
@@ -0,0 +1,64 @@
+****************************
+Overview & Credit
+****************************
+
+Minus the fact that MineSweeper is a game originating in the 1960's (as my father would say, "Before [I] was even a dirty thought") and the fact that the game itself was made by someone completely different and has evolved without my help, this is an original product. I mean to say, the code itself was not copied from a source and input into this document. This was a learning experience for me, so that would've defeated the purpose if I did that. That said, I believe ideas and inspiration are always worth giving credit to:
+
+• Microsoft - I played this game for hours on Windows 98, so thanks Billy and team for that.
+
+• The people who created the game and catered to its evolution for the past few decades - http://en.wikipedia.org/wiki/Minesweeper_(video_game) - thanks a milli.
+
+• Dan Purdy's Simon Says game tutorial was pretty instrumental in giving me the courage to build a JavaScript game on my own. The tutorial can be found on his site - http://www.danpurdy.co.uk/tutorial/simon-says-game-in-jquery-tutorial/ - I recommend you check it out, share it, and give him some accolades for it.
+
+• I found the box shadow effect on Paul Underwood's site - http://www.paulund.co.uk/creating-different-css3-box-shadows-effects - I changed it quite a bit over time I believe, but it's still a great help.
+
+• I can't for the life of me find where I pulled the rainbow css code from. I even googled the code itself and couldn't find it. Nonetheless, it was a great help. You can find multiple resources on linear gradients and color stops by googling just that or rainbow css (or by looking at the appropriate css fill/script tag within this repo).
+
+****************************
+Notes:
+****************************
+
+DELETE KEYWORD
+
+** Bugs:
+
+• Block 57 is not populating the correct number (check if conditionals in eachBlocksSurroundings() **SIDENOTE** change name (eachBlocksSurroundings)
+
+** Future Implementation
+
+CSS:
+• Create Explosion animation for each block
+• Create Explosion animation for entire board when bomb is clicked.
+
+JS:
+• Update flagMode
+• Update bombsLeft
+• Open connecting 0 blocks after each click.
+
+** Layout of game object:
+
+mineSweeper = {
+	<!-- active: BOOLEAN, -->
+	<!-- handler: BOOLEAN, -->
+	gameBlocks: ARRAY (NUMBERS 1 - 64),
+	bombs: ARRAY,
+	nonBombs: OBJECT (KEY = BLOCK NUMBER, VALUE = BOMBS TOUCHING IT),
+	<!-- playerClicks: ARRAY, -->
+	<!-- flagMode: BOOLEAN, -->
+	<!-- bombsLeft: INTEGER, -->
+	<!-- init: FUNCTION, -->
+	<!-- initBlockHandler: FUNCTION, -->
+	newGame: FUNCTION,
+	isItABomb: FUNCTION,
+	randomizeBombs: FUNCTION,
+	<!-- checkTopLeftBlock: FUNCTION, -->
+	dropBombs: FUNCTION,
+	populateNonBombs: FUNCTION,
+	eachBlocksSurroundings: FUNCTION,
+	<!-- flagBlock: FUNCTION, -->
+	<!-- blowOffScreen: FUNCTION, -->
+	<!-- logPlayerClicks: FUNCTION, -->
+	<!-- checkBlock: FUNCTION, -->
+	<!-- displayNumberOfBombsLeft: FUNCTION, -->
+
+}
---
+++ minesweeper.js
@@ -0,0 +1,280 @@
+var mineSweeper = { // game object
+	// active: true,
+	// handler: true,
+	gameBlocks: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64],
+	bombs: [],
+	nonBombs: {}, // { key = number of nonBomb block, value = how many bombs are near it.}
+	// playersClicks: [],
+	// flagMode: false, // depends on whether blocks can be clicked, whether to lay down the flag, and whether to update the bombsLeft property.
+	// bombsLeft: 10, // number of bombs left
+	// init: function() {
+	//     // if(this.handler===false) {
+	//     //   this.initBlockHandler();
+	//     // }
+	//     this.newGame();
+	// },
+	// initBlockHandler: function() {
+	    // var that = this;
+	    // $('.block').on('mouseup', function() {
+	      // if(that.active === true) {
+	        // var block=parseInt($(this).data('box'),10);
+	        // that.click($(this), block);
+	        // that.logPlayerSequence(block);
+	      // }
+	    // });
+	    // this.handler = true;
+	// },
+	newGame: function () {
+	    // this.displayNumberOfBombsLeft();
+	    this.randomizeBombs();
+	    this.dropBombs();
+	    this.populateNonBombs();
+	    this.eachBlocksSurroundings();
+	},
+	isItABomb: function(number) { // Register the block as clicked.
+		// var number = blockNumber.replace('.block', '');
+	    // var blockNumber = parseInt(block);
+	    if ($.inArray(number, mineSweeper['bombs']) != -1){
+	    	console.log(number + "true");
+	    	return true;
+	    } else {
+	    	console.log(number + "false");
+	    	return false;
+	    }
+	},
+	randomizeBombs: function() { // Randomize location of blocks.
+	    while(this.bombs.length < 10){
+	    	var randomNumber=Math.ceil(Math.random()*64);
+	    	var found=false;
+	    	for(var i=0; i<this.bombs.length; i++){
+	    		if(this.bombs[i]==randomNumber){
+	    			found=true;
+	    		}
+	    	}
+	    	if(!found)this.bombs[this.bombs.length]=randomNumber;
+	    }
+	},
+	// checkTopLeftBlock: function(val) {
+	// 	if ($.inArray(val-9,mineSweeper['bombs']) != -1){
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// },
+	dropBombs: function() {
+		$.each(this.bombs, function(_,val) {
+			$('.block'+val).text("Ω");
+		});
+		// that = this;
+	},
+	populateNonBombs: function() {
+		// var that = this;
+		$.each(this.gameBlocks, function(_,val){
+			if($.inArray(val, mineSweeper['bombs']) == -1){
+				mineSweeper['nonBombs'][val] = 0;
+			};
+		})
+
+		// $.each(this.nonBombs, function(val, num){
+		// 	$('.block'+val).text(val);
+		// })
+	},
+	eachBlocksSurroundings: function() {
+		$.each(this.nonBombs, function(val,_){
+			var bombs = 0;
+			var tempArray = [];
+			if (parseInt(val) === 1) { // for block 1
+				bombs = 0;
+				if ($.inArray(parseInt(val)+1,mineSweeper['bombs']) != -1) { 
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+8,mineSweeper['bombs']) != -1) {
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+9,mineSweeper['bombs']) != -1) {
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+				tempArray.push(val);
+			}
+			if (parseInt(val) > 1 && parseInt(val) < 8){ // for blocks 2, 3, 4, 5, 6, 7
+				bombs = 0;
+				if ($.inArray(parseInt(val)-1,mineSweeper['bombs']) != -1){
+					bombs++;
+				} 
+				if ($.inArray(parseInt(val)+1,mineSweeper['bombs']) != -1){
+					bombs++;
+				} 
+				if ($.inArray(parseInt(val)+7,mineSweeper['bombs']) != -1){
+					bombs++;
+				} 
+				if ($.inArray(parseInt(val)+8,mineSweeper['bombs']) != -1){
+					bombs++;
+				} 
+				if ($.inArray(parseInt(val)+9,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+				tempArray.push(val);
+			}
+			if (parseInt(val) == 8){ // for block 8
+				if ($.inArray(parseInt(val)-1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+7,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+				tempArray.push(val);
+			}
+			if (parseInt(val) % 8 == 0 && val != 8 && val != 64){ // for blocks 16, 24, 32, 40, 48, 56
+				if ($.inArray(parseInt(val)-9,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+7,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+				tempArray.push(val);
+			}
+			if ((parseInt(val)-1) % 8 == 0 && parseInt(val) != 57 && parseInt(val) != 1){ // for blocks 9, 17, 25, 33, 41, 49
+				if ($.inArray(parseInt(val)-8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-7,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+9,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+				tempArray.push(val);
+			}
+			if (parseInt(val) > 57 && parseInt(val) < 64){ // for blocks 58, 59, 60, 61, 62, 63
+				if ($.inArray(parseInt(val)-9,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-7,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+				tempArray.push(val);
+			}
+			if ($.inArray(val, tempArray) == -1){ 
+				if ($.inArray(parseInt(val)-9,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-7,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)-1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+1,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+7,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+8,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				if ($.inArray(parseInt(val)+9,mineSweeper['bombs']) != -1){
+					bombs++;
+				}
+				mineSweeper.nonBombs[val] = bombs;
+			}
+		});
+		
+		$.each(this.nonBombs, function(val, num){
+			$('.block'+val).text(num);
+		})
+	},
+	// flagBlock: function(block) {
+		// place a flag on the block
+		// do not allow a player to click this button (('.block').prop('disabled', true))
+	// },
+	// blowOffScreen: function(block) {
+		// individual block looks like it's blown off the screen
+		// if player lands on a bomb, all blocks blow off the screen.
+	// },
+	// logPlayersClicks: function() { // Log the blocks the player selected.
+	    
+	// },
+	// checkBlock: function() { // Check if block that was clicked is a bomb or not.
+	    
+	// },
+	// displayNumberOfBombsLeft: function() { // Updates number of bombs left on board
+	   
+	// },
+	// incorrectClick: function() { // Player clicked a bomb.
+	  
+	// },
+	  
+};
+
+// var gameBlock = {
+//   	blockNumber: 0,
+//  	bomb: false, // whether this block is a bomb
+//   	clicked: false,
+//   	squaresNextToIt: [],
+//   	bombsAroundIt: []
+// };
+
+$(document).ready(function(){
+  	$('.start').on('mousedown', function(){
+    	mineSweeper.newGame();
+    	$.each(mineSweeper.gameBlocks, function(_,val) {
+	  		$('.block'+val).on('mouseup', function(){
+	  			if (mineSweeper.isItABomb(val) == false){
+		  			$('.block'+val).css('background', 'white').css('color', 'black') //.css('border', '2px solid black');
+		  		} else {
+		  			$('.gameContainer').css('background', 'white').css('color','white');
+		  		}
+		  	});
+	  	});
+    });
+
+    $('.start').on('mouseup', function(){
+    	this.blur();
+  	});
+  	// $.each(mineSweeper.gameBlocks, function(_,val) {
+  	// 	$('.block'+val).on('mouseup', function(){
+	  // 		$('.block'+val).css('background', 'white');
+	  // 	});
+  	// })
+  	// $('.block').on('mouseup', function(){
+  	// 	$('.block').css('background', 'white');
+  	// });
+  
+});
